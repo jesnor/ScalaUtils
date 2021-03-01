21798e4dd657f8cbcad5361ce886b0a1307d1f1c
Jesper Nordenberg <jesper@nnl.se>
2021-03-01 12:04:27 +0100

    Better text formatting of float values

--- src/scala_utils/parameter/Parameter.scala
+++ src/scala_utils/parameter/Parameter.scala
@@ -8,7 +8,10 @@ class Parameter [T] (val group : Parameter_group,
                      val name : String,
                      val default : T,
                      val valid_values : Set Of T) extends CObservable_var (default) {
-  def value_string (v : T) : String = v.toString
+  def value_string (v : T) : String = v match {
+    case _: Float | _: Double => v.formatted("%.2f")
+    case _ => v.toString
+  }
 
   override protected def updated (vc : Value_change Of T) = {
     super.updated (vc)
