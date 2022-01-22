ec750bb187065592c5cc72486a7cb7b43e8a5001
Jesper Nordenberg <jesper.nordenberg@gmail.com>
2022-01-22 10:43:30 +0100

    Moved out Swing code
    Fixed Scala 3 errors

--- src/scala_utils/serialization/java_stream.scala
+++ src/scala_utils/serialization/java_stream.scala
@@ -5,27 +5,23 @@ import java.io.{IOException, InputStream, OutputStream}
 import scala_utils.utils.Of
 
 object java_stream {
-  implicit val byte_writer = new Writer [OutputStream, Byte] {
-    override def write (b : OutputStream, t : Byte) = b.write (t)
+  implicit val byte_writer : Writer [OutputStream, Byte] =
+    (b : OutputStream, t : Byte) => b.write (t)
+
+  implicit def byte_array_writer (implicit w : Writer [OutputStream, Int]) : Writer [OutputStream, Of [Array, Byte]] =
+    (b : OutputStream, t : Array [Byte]) => w.write (b, t.length)
+
+  implicit val byte_reader : Reader [InputStream, Byte] = (b : InputStream) => {
+    val v = b.read ()
+
+    if (v == -1)
+      throw new IOException ()
+
+    v.toByte
   }
 
-  implicit def byte_array_writer (implicit w : Writer [OutputStream, Int]) = new Writer [OutputStream, Array Of Byte] {
-    override def write (b : OutputStream, t : Array [Byte]) = w.write (b, t.length)
-  }
-
-  implicit val byte_reader = new Reader [InputStream, Byte] {
-    override def read (b : InputStream) = {
-      val v = b.read ()
-
-      if (v == -1)
-        throw new IOException ()
-
-      v.toByte
-    }
-  }
-
-  implicit def byte_array_reader (implicit r : Reader [InputStream, Int]) = new Reader [InputStream, Array [Byte]] {
-    override def read (b : InputStream) = {
+  implicit def byte_array_reader (implicit r : Reader [InputStream, Int]) : Reader [InputStream, Array [Byte]] =
+    (b : InputStream) => {
       val l = r read b
       val a = new Array [Byte](l)
 
@@ -34,5 +30,4 @@ object java_stream {
 
       a
     }
-  }
 }
--- src/scala_utils/serialization/little_endian.scala
+++ src/scala_utils/serialization/little_endian.scala
@@ -20,27 +20,21 @@ object little_endian {
   def read_int [B] (b : B, r : Reader [B, Byte]) = read_short (b, r) | (read_short (b, r) << 16)
   def read_long [B] (b : B, r : Reader [B, Byte]) = read_int (b, r).toLong | (read_int (b, r).toLong << 32)
 
-  implicit def le_short_writer [B] (implicit w : Writer [B, Byte]) = new Writer [B, Short] {
-    override def write (b : B, t : Short) = write_short (b, t, w)
-  }
+  implicit def le_short_writer [B] (implicit w : Writer [B, Byte]) : Writer [B, Short] =
+    (b : B, t : Short) => write_short (b, t, w)
 
-  implicit def le_int_writer [B] (implicit w : Writer [B, Byte]) = new Writer [B, Int] {
-    override def write (b : B, t : Int) = write_int (b, t, w)
-  }
+  implicit def le_int_writer [B] (implicit w : Writer [B, Byte]) : Writer [B, Int] =
+    (b : B, t : Int) => write_int (b, t, w)
 
-  implicit def le_long_writer [B] (implicit w : Writer [B, Byte]) = new Writer [B, Long] {
-    override def write (b : B, t : Long) = write_long (b, t, w)
-  }
+  implicit def le_long_writer [B] (implicit w : Writer [B, Byte]) : Writer [B, Long] =
+    (b : B, t : Long) => write_long (b, t, w)
 
-  implicit def le_short_reader [B] (implicit r : Reader [B, Byte]) = new Reader [B, Short] {
-    override def read (b : B) = read_short (b, r)
-  }
+  implicit def le_short_reader [B] (implicit r : Reader [B, Byte]) : Reader [B, Short] =
+    (b : B) => read_short (b, r)
 
-  implicit def le_int_reader [B] (implicit r : Reader [B, Byte]) = new Reader [B, Int] {
-    override def read (b : B) = read_int (b, r)
-  }
+  implicit def le_int_reader [B] (implicit r : Reader [B, Byte]) : Reader [B, Int] =
+    (b : B) => read_int (b, r)
 
-  implicit def le_long_reader [B] (implicit r : Reader [B, Byte]) = new Reader [B, Long] {
-    override def read (b : B) = read_long (b, r)
-  }
+  implicit def le_long_reader [B] (implicit r : Reader [B, Byte]) : Reader [B, Long] =
+    (b : B) => read_long (b, r)
 }
